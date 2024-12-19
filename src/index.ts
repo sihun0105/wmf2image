c63648e7d1919632664a0226a3fd55eefafc88a9
KIMSIHUN <tlgns14@nate.com>
2024-12-19 21:42:38 +0900

    🎨

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-export const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
-const data = greet("world");
-console.log(data);
+import path from "path";
+import fs from "fs";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const filePath = path.resolve(__dirname, "..", "public", "sample.wmf");
+
+console.log("File Path:", filePath);
+
+if (fs.existsSync(filePath)) {
+  console.log("파일이 존재합니다.");
+} else {
+  console.log("파일이 존재하지 않습니다.");
+}
+
+fs.readFile(filePath, (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
---
+++ src/types/custom.d.ts
@@ -0,0 +1,4 @@
+declare module "*.wmf" {
+  const value: string;
+  export default value;
+}
